c04f82e84810a3eaceeb7b405c5a943ea3788599
이광호 <leekh4232@gmail.com>
2024-03-12 00:29:08 +0900

    .

--- hossam/timeserise.py
+++ hossam/timeserise.py
@@ -1,6 +1,6 @@
-from pandas import DataFrame
+from pandas import DataFrame, Series
 from statsmodels.tsa.stattools import adfuller
-
+from matplotlib import pyplot as plt
 from .util import my_pretty_table
 from .plot import my_lineplot
 
@@ -73,3 +73,128 @@ def my_diff(
             break
 
     return df
+
+
+def my_rolling(
+    data: Series,
+    window: int,
+    plot: bool = True,
+    figsize: tuple = (10, 5),
+    dpi: int = 100,
+) -> Series:
+    """이동평균을 계산한다.
+
+    Args:
+        data (Series): 시리즈 데이터
+        window (int): 이동평균 계산 기간
+        plot (bool, optional): 이동평균 그래프 표시 여부. Defaults to True.
+        figsize (tuple, optional): 그래프 크기. Defaults to (10, 5).
+        dpi (int, optional): 그래프 해상도. Defaults to 100.
+
+    Returns:
+        Series: 이동평균 데이터
+    """
+    rolling = data.rolling(window=window).mean()
+    df = DataFrame(
+        {
+            rolling.name: rolling,
+        },
+        index=rolling.index,
+    )
+
+    if plot:
+        my_lineplot(
+            df=df,
+            yname=rolling.name,
+            xname=df.index,
+            figsize=figsize,
+            dpi=dpi,
+            callback=lambda ax: ax.set_title(f"Rolling (window={window})"),
+        )
+
+    return rolling
+
+
+def my_ewm(
+    data: Series, span: int, plot: bool = True, figsize: tuple = (10, 5), dpi: int = 100
+) -> Series:
+    """지수가중이동평균을 계산한다.
+
+    Args:
+        data (Series): 시리즈 데이터
+        span (int): 지수가중이동평균 계산 기간
+        plot (bool, optional): 지수가중이동평균 그래프 표시 여부. Defaults to True.
+        figsize (tuple, optional): 그래프 크기. Defaults to (10, 5).
+        dpi (int, optional): 그래프 해상도. Defaults to 100.
+
+    Returns:
+        Series: 지수가중이동평균 데이터
+    """
+    ewm = data.ewm(span=span).mean()
+
+    df = DataFrame(
+        {
+            ewm.name: ewm,
+        },
+        index=ewm.index,
+    )
+
+    if plot:
+        my_lineplot(
+            df=df,
+            yname=ewm.name,
+            xname=df.index,
+            figsize=figsize,
+            dpi=dpi,
+        )
+
+    return ewm
+
+
+def my_seasonal_decompose(
+    data: Series,
+    model: str = "additive",
+    plot: bool = True,
+    figsize: tuple = (10, 5),
+    dpi: int = 100,
+):
+    """시계열 데이터를 계절적, 추세적, 불규칙적 성분으로 분해한다.
+
+    Args:
+        data (Series): 시리즈 데이터
+        model (str, optional): 분해 모델. "additive" 또는 "multiplicative". Defaults to "additive".
+        plot (bool, optional): 분해 결과 그래프 표시 여부. Defaults to True.
+        figsize (tuple, optional): 그래프 크기. Defaults to (10, 5).
+        dpi (int, optional): 그래프 해상도. Defaults to 100.
+    """
+    from statsmodels.tsa.seasonal import seasonal_decompose
+
+    sd = seasonal_decompose(data, model=model)
+
+    sd_df = DataFrame(
+        {
+            "original": sd.observed,
+            "trend": sd.trend,
+            "seasonal": sd.seasonal,
+            "resid": sd.resid,
+        },
+        index=data.index,
+    )
+
+    if plot:
+        figure = sd.plot()
+        figure.set_size_inches((figsize[0], figsize[1] * 4))
+        figure.set_dpi(dpi)
+
+        fig, ax1, ax2, ax3, ax4 = figure.get_children()
+
+        ax1.set_ylabel("Original")
+        ax1.grid(True)
+        ax2.grid(True)
+        ax3.grid(True)
+        ax4.grid(True)
+
+        plt.show()
+        plt.close()
+
+    return sd_df

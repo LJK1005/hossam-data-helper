5f0d39307742587b26e783fcbc0bf7ed7ba9f87c
leekh <leekh4232@gmail.com>
2024-03-31 17:20:08 +0900

    update 2024-03-31 17:19:55.78

--- hossam/cluster.py
+++ hossam/cluster.py
@@ -9,7 +9,7 @@ from sklearn.cluster import KMeans
 from hossam.plot import my_lineplot
 
 
-def _get_inertia(
+def __kmeans(
     data: DataFrame,
     n_clusters: int,
     init: Literal["k-means++", "random"] = "k-means++",
@@ -17,7 +17,7 @@ def _get_inertia(
     random_state=0,
     algorithm: Literal["lloyd", "elkan", "auto", "full"] = "lloyd",
 ) -> float:
-    """이너셔 값을 계산한다.
+    """KMmeans 알고리즘을 수행한다.
 
     Args:
         data (DataFrame): 원본 데이터
@@ -38,16 +38,21 @@ def _get_inertia(
         algorithm=algorithm,
     )
     kmeans.fit(data)
-    return (kmeans.inertia_, n_clusters)
+    kmeans.n_clusters = n_clusters
+    return kmeans
 
 
-def my_inertia(
+def my_kmeans(
     data: DataFrame,
     max_clusters: int | list = 10,
     init: Literal["k-means++", "random"] = "k-means++",
     max_iter: int = 500,
     random_state=0,
     algorithm: Literal["lloyd", "elkan", "auto", "full"] = "lloyd",
+    scoring: Literal["inertia", "silhouette"] = "inertia",
+    plot: bool = True,
+    figsize: tuple = (10, 5),
+    dpi: int = 100,
 ) -> DataFrame:
     """클러스터 개수에 따른 이너셔 값을 계산한다.
 
@@ -67,7 +72,7 @@ def my_inertia(
         for n_clusters in range(2, max_clusters + 1):
             results.append(
                 executor.submit(
-                    _get_inertia,
+                    __kmeans,
                     data,
                     n_clusters,
                     init,
@@ -77,83 +82,52 @@ def my_inertia(
                 )
             )
 
-        return_values = [r.result() for r in futures.as_completed(results)]
-        df = DataFrame(return_values, columns=["inertia", "n_clusters"])
-        df.set_index("n_clusters", inplace=True)
-        df.sort_index(inplace=True)
-        return df
+        kmeans_list = [r.result() for r in futures.as_completed(results)]
 
+        if scoring == "inertia":
+            inertia = [k.inertia_ for k in kmeans_list]
+            n_clusters = [k.n_clusters for k in kmeans_list]
 
-def my_knn_elbow(
-    data: DataFrame,
-    max_clusters: int | list = 10,
-    init: Literal["k-means++", "random"] = "k-means++",
-    max_iter: int = 500,
-    random_state=0,
-    algorithm: Literal["lloyd", "elkan", "auto", "full"] = "lloyd",
-    plot: bool = True,
-    figsize: tuple = (10, 5),
-    dpi: int = 100,
-) -> float:
-    """KNN을 이용한 최적 클러스터 개수를 찾는다.
-
-    Args:
-        data (DataFrame): 원본 데이터
-        max_clusters (int | list, optional): 최대 클러스터 개수. 정수로 전달할 경우 `2`부터 주어진 개수까지 반복 수행한다. Defaults to 10.
-        init (Literal["k-means++", "random"], optional): 초기화 방법. Defaults to "k-means++".
-        max_iter (int, optional): 최대 반복 횟수. Defaults to 500.
-        random_state (int, optional): 난수 시드. Defaults to 0.
-        algorithm (Literal["lloyd", "elkan", "auto", "full"], optional): 알고리즘. Defaults to "lloyd".
-        plot (bool, optional): 엘보우포인트 그래프 표시 여부. Defaults to True.
-        figsize (tuple, optional): 그래프 크기. Defaults to (10, 5).
-        dpi (int, optional): 그래프 해상도. Defaults to 100.
-
-    Returns:
-        float: _description_
-    """
-    inertia = my_inertia(
-        data=data,
-        max_clusters=max_clusters,
-        init=init,
-        max_iter=max_iter,
-        random_state=random_state,
-        algorithm=algorithm,
-    )
-    kn = KneeLocator(
-        x=inertia.index, y=inertia.inertia, curve="convex", direction="decreasing"
-    )
-    best_k = kn.elbow
-    best_y = kn.elbow_y
-
-    if plot:
-
-        def hvline(ax):
-            ax.set_ylabel("inertia")
-            ax.set_xlabel("cluster count")
-            ax.set_title("Elbow Method")
-            ax.axhline(best_y, color="red", linestyle="--", linewidth=0.7)
-            ax.axvline(best_k, color="red", linestyle="--", linewidth=0.7)
-            ax.text(
-                best_k + 0.2,
-                best_y + 0.2,
-                f"k={best_k}",
-                fontsize=20,
-                color="red",
-                va="bottom",
-                ha="left",
+            kn = KneeLocator(
+                x=n_clusters,
+                y=inertia,
+                curve="convex",
+                direction="decreasing",
             )
 
-        my_lineplot(
-            df=inertia,
-            xname=inertia.index,
-            marker="o",
-            linewidth=2,
-            yname="inertia",
-            figsize=figsize,
-            dpi=dpi,
-            callback=hvline,
-        )
+            best_k = kn.elbow
+            best_y = kn.elbow_y
+            best_model = next(filter(lambda x: x.n_clusters == best_k, kmeans_list))
 
-    inertia["best"] = False
-    inertia.loc[best_k, "best"] = True
-    return inertia
+            if plot:
+
+                def hvline(ax):
+                    ax.set_ylabel("inertia")
+                    ax.set_xlabel("cluster count")
+                    ax.set_title("Elbow Method")
+                    ax.axhline(best_y, color="red", linestyle="--", linewidth=0.7)
+                    ax.axvline(best_k, color="red", linestyle="--", linewidth=0.7)
+                    ax.text(
+                        best_k + 0.2,
+                        best_y + 0.2,
+                        f"k={best_k}",
+                        fontsize=20,
+                        color="red",
+                        va="bottom",
+                        ha="left",
+                    )
+
+                my_lineplot(
+                    df=None,
+                    xname=n_clusters,
+                    yname=inertia,
+                    marker="o",
+                    linewidth=2,
+                    figsize=figsize,
+                    dpi=dpi,
+                    callback=hvline,
+                )
+
+            return best_model
+        else:
+            return sorted(kmeans_list, key=lambda x: x.n_clusters)
